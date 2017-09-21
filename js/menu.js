5bd4589662cd2d05645545eec3c28173be7384cd
dabbe98 <31295349+dabbe98@users.noreply.github.com>
2017-09-21 12:48:20 +0200

    Intro javascript

---
+++ js/menu.js
@@ -0,0 +1,19 @@
+consloe.log("woria ta inte min cyckel");
+console.log("på min mamma jag kommer skjuta dig")
+
+var text1="blub";
+var text1='blub';
+var number = 23; 
+var decNumber = 98.0922;
+var boolean1 = false;
+
+console.log(boolean1 + " " + text1 + decNumber);
+console.log(number1 + decNumber);
+
+function blub() {
+	console.log("The blub function!");
+}
+
+function blub2(var parm) {
+	console.log(parm + text1);
+}
